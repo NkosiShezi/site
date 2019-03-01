44432020aed340d689fc853c190d3b1c0dcbde18
Nkosi <nkosi@efx-capital.com>
2019-03-01 15:38:42 +0200

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,8 +18,21 @@
         background-position: center;
         min-height: 1000px;
       }
+      .section {
+            padding-top:4vw;
+            padding-bottom:4vw;
 
-      @media screen and (max-width: 678px) {
+      }
+
+      .tabs .indicator {
+            background-color:#3f51b5
+      }
+
+      .tabs .tab a:hover, .tabs .tab a.active {
+            background-color: transparent;
+      }
+
+      @media screen and (max-width: 670px) {
           header{
               min-height:500px
           }
@@ -36,6 +49,9 @@
         <nav class="nav-wrapper transparent" >
             <div class="container">
                 <a href="" class="brand-logo">Photo Ninja</a>
+                <a href="" class="sidenav-trigger" data-target="mobile-menu">
+                    <i class="material-icons">menu</i>
+                </a>
                 <ul class="right hide-on-med-and-down">
                     <li><a href="#">Photos</a></li>
                     <li><a href="#">Services</a></li>
@@ -51,14 +67,100 @@
     </header>
 
   <!-- photo / grid -->
+  <section class="container section" id="photos">
+      <div class="row">
+            <div class="col s12 l4">
+                <img src="{{URL::asset('img/portrait.jpg')}}" alt="" class="materialboxed responsive-img">
+            </div>
+            <div class="col s12 l6 offset-l1">
+                <h2 class="indigo-text text-darken-4">Portrait</h2>
+                <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+            </div>
+      </div>
+      <div class="row">
+            <div class="col s12 l4 push-l7 offset-l1">
+                <img src="{{URL::asset('img/city.jpg')}}" alt="" class="materialboxed responsive-img">
+            </div>
+            <div class="col s12 l6 pull-l5 right-align offset-l1">
+                <h2 class="indigo-text text-darken-4">City</h2>
+                <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+            </div>
+      </div>
+      <div class="row">
+            <div class="col s12 l4">
+                <img src="{{URL::asset('img/nature.jpg')}}" alt="" class="materialboxed responsive-img">
+            </div>
+            <div class="col s12 l6 offset-l1">
+                <h2 class="indigo-text text-darken-4">Nature</h2>
+                <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+            </div>
+      </div>
+  </section>  
+
+
 
   <!-- parallax -->
+    <div class="parallax-container">
+      <div class="parallax">
+        <img src="{{URL::asset('img/street.jpg')}}" alt="" class="resposive-img">
+      </div>
+    </div>
+    
 
   <!-- services / tabs -->
 
+  <section class="container section" id="services">
+      <div class="row">
+            <div class="col s12 l4">
+                <h2 class="indigo-text text-darken-4">What i do...</h2>
+                <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+                <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+            </div>
+            <div class="col s1 l5 offset-l2">
+                <ul class="tabs">
+                    <li class="tab col s6">
+                        <a href="#photography" class="indigo-text text-darken-4">Photography</a>
+                    </li>
+                    <li class="tab col s6">
+                        <a href="#editing" class="indigo-text text-darken-4">Editting</a>
+                    </li>
+                </ul>
+                <div class="col s12" id="photography">
+                    <p class="flow-text indigo-text text-darken-4">PHOTOGRAPHY</p>
+                    <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+                    <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p> 
+                </div>
+                <div class="col s12" id="editing">
+                    <p class="flow-text indigo-text text-darken-4">EDITING</p>
+                    <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+                    <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p> 
+                </div>
+            </div>
+      </div>
+      
+  </section>
+
   <!-- parallax -->
 
+ <div class="parallax-container">
+      <div class="parallax">
+        <img src="{{URL::asset('img/stars.jpg')}}" alt="" class="resposive-img">
+      </div>
+    </div>
+    
   <!-- contact form -->
+
+  <div class="container">
+      <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+      <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+      <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+      <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+      <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+      <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+      <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+      <p>Cras volutpat mattis iaculis. Mauris ullamcorper porttitor metus. Morbi pretium eget lacus id accumsan</p>
+  </div>
+
 
   <!-- footer -->
 
@@ -67,6 +169,11 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
   <script>
     $(document).ready(function(){
+          $('.sidenav').sidenav();  
+          $('.materialboxed').materialbox();  
+          $('.parallax').parallax();
+          $('.tabs').tabs();
+
     });
   </script>
 </body>
